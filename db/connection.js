e2dfdc277deb5c2ffd8bbb6684abac125d36e037
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2024-08-09 22:30:33 +0800

    db connection and create table

--- db/connection.js
+++ db/connection.js
@@ -1,42 +1,41 @@
-import pg from 'pg'
-import { createUserTable } from "../models/user.js";
-const { Pool } = pg
+import pg from "pg";
+import { createAllTables} from "../models/tableCreate.js";
 
+
+const { Pool } = pg;
 
 //connection with database
 const pool = new Pool({
-  host: '127.0.0.1',
-  user: 'postgres',
-  password: '12345',
-  database: 'intercity_bus',
+  host: "127.0.0.1",
+  user: "postgres",
+  password: "12345",
+  database: "intercity_bus",
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-})
+});
 
 async function testConnection() {
-    //try-catch block
-    try{
-        
-        const dbName = await pool.query('SELECT current_database()');
-        const dbRes = await pool.query('SELECT NOW()');
-        const time = dbRes.rows[0].now;
-        const name = dbName.rows[0].current_database;
+  //try-catch block
+  try {
+    const dbName = await pool.query("SELECT current_database()");
+    const dbRes = await pool.query("SELECT NOW()");
+    const time = dbRes.rows[0].now;
+    const name = dbName.rows[0].current_database;
 
-        console.log(`Database connection successful with ${name}'s database at ${time}`);
+    console.log(
+      `Database connection successful with ${name}'s database at ${time}`
+    );
 
-        await createUserTable();
-        
-    }
-    catch(err){
-        console.log('Database connection failed');
-        console.log(err);
-    }
-    
+    await createAllTables();
+
+  } catch (err) {
+    console.log("Database connection failed");
+    console.log(err);
   }
+}
 
 export { pool, testConnection };
-
 
 //Promise 101
 // promise is variable for future value
@@ -48,4 +47,3 @@ export { pool, testConnection };
 // try-catch block
 // try - block of code pending and resolved
 // catch - block of code rejected
-
